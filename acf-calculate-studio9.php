f604d0c0a0b5c465de2b8fbaf172a3549bec0caa
Amir Abbas <amirabbas750@gmail.com>
2022-07-26 20:48:00 +0430

    add button print

--- acf-calculate-studio9.php
+++ acf-calculate-studio9.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name:      محاسبه مجموع هزینه در افزونه acf
  * Plugin URI:        https://webslash.ir/portfolio/acf-calculate
@@ -12,10 +13,18 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       wb-quiz
  */
-function acf_calculate_studio9_script() {
-    wp_enqueue_script( 'wordifyfa', plugins_url('acf-calculate-studio9') . '/asset/wordifyfa.js', array( 'jquery' ) );
-    wp_enqueue_script( 'acf-calculate-studio', plugins_url('acf-calculate-studio9') . '/asset/script.js', array( 'jquery' ) );
+require_once(ABSPATH . '/wp-admin/includes/plugin.php');
+function acf_calculate_studio9_script()
+{
+    wp_enqueue_script('wordifyfa' , plugins_url('acf-calculate-studio9') . '/asset/wordifyfa.js' , ['jquery']);
+    wp_enqueue_script('acf-calculate-studio' , plugins_url('acf-calculate-studio9') . '/asset/script.js' , ['jquery']);
 }
-add_action( 'wp_enqueue_scripts', 'acf_calculate_studio9_script' );
-add_action( 'admin_enqueue_scripts', 'acf_calculate_studio9_script' );
 
+add_action('wp_enqueue_scripts' , 'acf_calculate_studio9_script');
+add_action('admin_enqueue_scripts' , 'acf_calculate_studio9_script');
+add_action('acf_frontend/after_form' , function($post)
+{
+    ?>
+    <button type="button" class=" button" data-state="publish" onclick="window.print()">Print</button>
+    <?php
+});
