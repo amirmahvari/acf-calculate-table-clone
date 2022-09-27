64ba6452bfcd6c947ca70f7f84ffa33f9611d42f
Amir Abbas <amirabbas750@gmail.com>
2022-09-27 12:56:32 +0330

    seperator

--- asset/script.js
+++ asset/script.js
@@ -17,44 +17,44 @@ jQuery(document).ready(function () {
         el.parents('tr').find('.acf-tr-service-price input').val(el.val())
         calculate(el.parents('tr'))
     })
-    jQuery(document).on('keyup', '.acf-tr-service-price input',function () {
+    jQuery(document).on('keyup change', '.acf-tr-service-price input',function () {
         let el = jQuery(this);
         calculate(el.parents('tr'))
     })
-    jQuery(document).on('keyup', '.acf-tr-service-quantity input',function () {
+    jQuery(document).on('keyup change', '.acf-tr-service-quantity input',function () {
         let el = jQuery(this);
         calculate(el.parents('tr'))
     })
-    jQuery(document).on('keyup', '.acf-tr-service-discount input',function () {
+    jQuery(document).on('keyup change', '.acf-tr-service-discount input',function () {
         let el = jQuery(this);
         calculate(el.parents('tr'))
     })
 
-    jQuery(document).on('keyup', '.acf-prepayment input',function () {
+    jQuery(document).on('keyup change', '.acf-prepayment input',function () {
         sum()
     })
-    jQuery(document).on('keyup', '.acf-discount-percent input',function () {
+    jQuery(document).on('keyup change', '.acf-discount-percent input',function () {
         sum()
     })
-    jQuery(document).on('keyup', '.acf-discount-fixed input',function () {
+    jQuery(document).on('keyup change', '.acf-discount-fixed input',function () {
         sum()
     })
-    jQuery(document).on('keyup', '.acf-tax input',function () {
+    jQuery(document).on('keyup change', '.acf-tax input',function () {
         sum()
     })
-    jQuery(document).on('keyup', '.acf-tax-added input',function () {
+    jQuery(document).on('keyup change', '.acf-tax-added input',function () {
         sum()
     })
-    jQuery(document).on('keyup', '.acf-total input',function () {
+    jQuery(document).on('keyup change', '.acf-total input',function () {
         sum()
     })
-    jQuery(document).on('keyup', '.acf-total-alpha input',function () {
+    jQuery(document).on('keyup change', '.acf-total-alpha input',function () {
         sum()
     })
     jQuery(document).on('change', '.acf-status input',function () {
         sum()
     })
-    jQuery(document).on('keyup',   '.acf-changer input',function () {
+    jQuery(document).on('keyup change',   '.acf-changer input',function () {
         let el = jQuery(this)
         if (!el.value){
             jQuery('.acf-items-changer input').prop('checked',false)
@@ -98,9 +98,67 @@ jQuery(document).ready(function () {
         }
         if (!jQuery('.acf-prepayment').hasClass('acf-hidden')) {
             let prepayment = jQuery('.acf-prepayment input');
-            final -= prepayment.val()
+            final -= prepayment.val().replace(/,/g, "")
         }
         total.val(final)
         totalAlpha.val(wordifyRialsInTomans(final * 10))
+        sperator()
+    }
+    function sperator() {
+        jQuery(document).ready(function ($) {
+            $('.acf-price input').focusin(function () {
+                var price = $(this).val();
+                price = price.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+                $(this).val(price);
+            });
+            $('.acf-price input').focus(function () {
+                var price = $(this).val();
+                price = price.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+                $(this).val(price);
+            });
+            $('.acf-price input').on('input', function() {
+                var price = $(this).val();
+                price = price.replace(/,/g, '');
+                price = price.replace(/[^\d]+/g,'');
+                var pn = ["Û°", "Û±", "Û²", "Û³", "Û´", "Ûµ", "Û¶", "Û·", "Û¸", "Û¹"];
+                var en = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+                var cache = price;
+                for (var i = 0; i < 10; i++) {
+                    var regex_fa = new RegExp(pn[i], 'g');
+                    cache = cache.replace(regex_fa, en[i]);
+                }
+                price = cache;
+                price = price.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+                $(this).val(price);
+            });
+            $('.acf-price input').focusout(function () {
+                var price = $(this).val();
+                price = price.replace(/,/g, '');
+                $(this).val(price);
+            });
+
+            $('#acff-post-field_6167df2a67bc5').change(function () {
+                $('#acff-post-field_6167e0945188b').val('');
+                $('#acf-post-field_6167dfcb26160').val('');
+            });
+
+            $('#acf-field_6167df2a67bc5').change(function () {
+                $('#acf-field_6167e0945188b').val('');
+                $('#acf-field_6167dfcb26160').val('');
+            });
+
+            $(' .price-label h6').each(function() {
+                var price = $(this).text();
+                price = price.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+                $(this).text(price);
+            });
+
+            $(' .price-label h5').each(function() {
+                var price = $(this).text();
+                price = price.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+                $(this).text(price);
+            });
+
+        })
     }
 })
